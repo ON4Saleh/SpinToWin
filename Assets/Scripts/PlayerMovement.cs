eabe41b9de3acd1bf1149813eaeba1df0fa04230
ON4Saleh <116005799+ON4Saleh@users.noreply.github.com>
2025-01-15 00:37:43 +0200

    1st Commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
-
+using UnityEngine.AI;
+using System.Collections;
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Transform target;
+    private NavMeshAgent navMeshAgent;
+    Ray lastRay;
     void Start()
-    {   
-     //   sdsda
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButton(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction);
+        if (target != null) 
+        {
+            navMeshAgent.destination = target.position;
+        }
     }
 }
