1b68e2f2ae93885e2fb649070d8ca4b703152da9
Omar Salem <omar.ghandy780@gmail.com>
2025-01-29 16:21:55 +0200

    camera movment

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    [SerializeField] Transform target;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Camera camera = GetComponent<Camera>();
+    private Vector3 offset;
+    [SerializeField] private Transform target;
+    [SerializeField ]private float smoothTime;
+    private Vector3 currentVelocity = Vector3.zero;
+
+    private void Awake() {
+        offset = transform.position - target.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = target.position;
+    private void LateUpdate() {
+        Vector3 targetPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
     }
 }
